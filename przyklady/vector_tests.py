d940686089b6c198da24e043e37d4e9852dea6d4
piotrszleg <szlegp@gmail.com>
2020-12-13 21:43:20 +0100

    Better normalized function tests. length jako property.

--- przyklady/vector_tests.py
+++ przyklady/vector_tests.py
@@ -11,49 +11,55 @@ def close(a, b):
     EPSILON=0.001
     return abs(a-b)<EPSILON
 
+def division_or_zero(divided, divider):
+    if divider==0:
+        return 0
+    else:
+        return divided/divider
+
 class Vector:
     def __init__(self, x, y):
         self.x=x
         self.y=y
     
+    @property
     def length(self):
         return sqrt(self.x*self.x+self.y*self.y)
 
-    def unit(self):
-        length=self.length()
-        # oczywiście tutaj jest błąd - brak sprawdzenia length!=0
-        # niestety testy tego nie złapały co tylko jeden rodzaj testów nie wystarcza
+    def normalized(self):
+        length=self.length
+        # celowy bład który będziemy próbować złapać
+        # if length==0:
+        #     return Vector(1, 0)
         return Vector(self.x/length, self.y/length)
 
     def __eq__(self, other):
         return close(self.x, other.x) and close(self.y, other.y)   
 
 print("chosen points test:")
-assert(close(Vector(3, 4).length(), 5))
-assert(close(Vector(1, 1).length(), sqrt(2)))
-assert(Vector(1, 0).unit()==Vector(1, 0))
-assert(close(Vector(2, 0).length(), 2))
+assert(close(Vector(3, 4).length, 5))
+assert(close(Vector(1, 1).length, sqrt(2)))
+assert(Vector(1, 0).normalized()==Vector(1, 0))
+assert(close(Vector(100, 5).normalized().length, 1))
+assert(close(Vector(2, 0).length, 2))
 
 print("random tests:")
 for i in range(1000):
     def random_coordinate():
         value=random()
         multiplier=1000
-        # change this to value<0.1 to catch (0, 0)
+        # po zmianie na value<0.1 przypadek (0, 0) zostanie złapany
         if value<0.01:
             return 0
         return (value-0.5)*1000
     x=random_coordinate()
     y=random_coordinate()
     print(f"test x={x} \t y={y}")
-    
-    def vector_length(sign_x, sign_y):
-        return Vector(sign_x*x, sign_y*y).length()
-
-    def unit_vector_length(sign_x, sign_y):
-        return Vector(sign_x*x, sign_y*y).unit().length()
 
     signs=[(1, 1), (-1, 1), (1, -1), (-1, -1)]
+    
+    def vector_length(sign_x, sign_y):
+        return Vector(sign_x*x, sign_y*y).length
 
     def all_close(previous, current):
         # ważne, przy operacjach na floatach występują błedy
@@ -62,11 +68,18 @@ for i in range(1000):
 
     # wektory o różnych znakach x, y powinny mieć tą samą długość
     reduce(all_close, starmap(vector_length, signs))
-    # wektor jednostkowy ma długość jeden
-    # tutaj testujemy też przejście przez metody pomocnicze
-    reduce(all_close, starmap(unit_vector_length, signs))
+    
+    for sign_x, sign_y in signs:
+        original=Vector(sign_x*x, sign_y*y)
+        normalized=original.normalized()
+        # length of one
+        assert(close(normalized.length, 1))
+        # same direction
+        assert(close(
+            division_or_zero(original.x, original.y), 
+            division_or_zero(normalized.x, normalized.y)))
 
 print("characteristic points test:")
-assert(Vector(0, 1).unit().length()==1)
-assert(Vector(1, 0).unit().length()==1)
-assert(Vector(0, 0).unit().length()==1)
+assert(Vector(0, 1).normalized().length==1)
+assert(Vector(1, 0).normalized().length==1)
+assert(Vector(0, 0).normalized().length==1)
